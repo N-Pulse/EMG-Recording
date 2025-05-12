6c18b31497dd853b4867832b49c8a1dbbe5bf9ef
Will1618sd <101874161+Will1618sd@users.noreply.github.com>
2025-05-12 16:48:26 +0200

    recording protocol for 3 channels

--- recording_protocol_change_encoding_button.py
+++ recording_protocol_change_encoding_button.py
@@ -3,18 +3,19 @@ import time
 import csv
 import serial
 import pandas as pd
+import numpy as np
 import keyboard
 from datetime import datetime
 
 # Setup serial connection (Modify COM port as needed)
-SERIAL_PORT = "COM4"  # Change this for your system
+SERIAL_PORT = "COM7"  # Change this for your system
 BAUD_RATE = 115200
 
 def create_folder(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-def record_data(sequence, file_path, duration=3, transition_time=3):
+def record_data(sequence, file_path, nb_channels, duration=3, transition_time=3):
     try:
         # filename = f"{participant}_{hand}_{position}_{sequence}_{timestamp}.csv"
         # file_path = os.path.join(base_folder, filename)
@@ -24,26 +25,33 @@ def record_data(sequence, file_path, duration=3, transition_time=3):
         ser.flushInput()
 
         print(f"You will keep each position during {duration} seconds.")
-        time.sleep(1)
+        # time.sleep(1)
         print(f"You will have to press the space while you change position.")
-        time.sleep(1)
+        # time.sleep(1)
         print(f"It is enough time, don't rush...")
         time.sleep(1)
         input(f"Press ENTER to start sequence...")
         countdown()
         
         with open(file_path, mode='w', newline='') as file:
+            header = ["Timestamp"]
+            for i in range(nb_channels):
+                header.append(f"Channel{i+1}")
+            header.append("Action1", "Action2")
             writer = csv.writer(file)
-            writer.writerow(["Timestamp", "Channel1", "Channel2", "Action1", "Action2"])  # Modify headers as needed
+            writer.writerow(header)  # Modify headers as needed
             
             print("Waiting for first valid data...")
             first_data = None
             while first_data is None:
                 raw_data = ser.readline().decode('utf-8').strip()
-                print(raw_data)
-                channel1, channel2 = float(raw_data.split(" ")[0]), float(raw_data.split(" ")[1])
-                if isinstance(channel1, float) and isinstance(channel2, float):  # Ensure it's valid numeric data
-                    first_data = [channel1, channel2]
+                channels = []
+                for i in range(nb_channels):
+                    channels.append(float(raw_data.split(" ")[i]))
+                
+                if [isinstance(channel, float) for channel in channels].all():
+                # if isinstance(channel1, float) and isinstance(channel2, float):  # Ensure it's valid numeric data
+                    first_data = channels
 
             start_sequence_time = time.time()
 
@@ -57,11 +65,14 @@ def record_data(sequence, file_path, duration=3, transition_time=3):
                     current_time = round(time.time() - start_sequence_time, 3)
                     data = ser.readline().decode('utf-8').strip()
 
+                    channels = []
+
                     if not data:
                         continue
 
                     try:
-                        channel1, channel2 = float(data.split(" ")[0]), float(data.split(" ")[1])
+                        for i in range(nb_channels):
+                            channels.append(float(data.split(" ")[i]))
                     except ValueError:
                         continue
 
@@ -70,12 +81,18 @@ def record_data(sequence, file_path, duration=3, transition_time=3):
                         print(f"Press and hold SPACE to transition to '{sequence[i+1]}'...")
                         prompted = True
 
+                    buffer = [current_time]
+                    for channel in channels:
+                        buffer.append(channel)
+                    buffer.append(sequence[i])
+
                     # Read spacebar state
                     if keyboard.is_pressed('space'):
                         transition_started = True
-                        writer.writerow([current_time, channel1, channel2, sequence[i], sequence[i+1]])
+                        buffer.append(sequence[i+1])
+                        writer.writerow(buffer)
                     else:
-                        writer.writerow([current_time, channel1, channel2, sequence[i], None])
+                        writer.writerow(buffer)
                         if transition_started:
                             print(f"Transition complete. Starting '{sequence[i+1]}'\n")
                             break
@@ -116,16 +133,30 @@ transition_time=3
 file_path = "EMG_recording_protocol.xlsx"
 xls = pd.ExcelFile(file_path)
 df_sequences = pd.read_excel(xls, sheet_name="Sequences")
+nb_of_sequences = np.shape(df_sequences)[1]
 
 # Extract sequences
-sequence_1 = df_sequences.iloc[:, 0].dropna().tolist()
-sequence_2 = df_sequences.iloc[:, 1].dropna().tolist()
-sequence_3 = df_sequences.iloc[:, 2].dropna().tolist()
-sequence_4 = df_sequences.iloc[:, 3].dropna().tolist()
+sequences = {}
+for seq in range(nb_of_sequences):
+    sequences[seq]=df_sequences.iloc[:,seq].dropna().tolist()
+
+# sequence_1 = df_sequences.iloc[:, 0].dropna().tolist()
+# sequence_2 = df_sequences.iloc[:, 1].dropna().tolist()
+# sequence_3 = df_sequences.iloc[:, 2].dropna().tolist()
+# sequence_4 = df_sequences.iloc[:, 3].dropna().tolist()
+
+# Nb of channels used
+nb_channels = 0
+while nb_channels not in range(1, 10):
+    nb_channels = input("Enter number of channels")
 
 # User inputs
 participant = input("Enter participant initials: ")
-hand = input("Enter which hand (L or R): ").upper()
+
+hand = 0
+while hand not in ['L', 'R']:
+    hand = input("Enter which hand (L or R): ").upper()
+
 # position = input("Enter position (1-5): ")
 
 # Create necessary folders
@@ -139,23 +170,16 @@ print("1 - Single DOF Movements")
 print("2 - Hand Poses")
 print("3 - Grasps")
 print("4 - Simple Gestures")
-sequence_choice = int(input("Enter sequence number (1-4): "))
+print("5 - Single DOF")
+
+sequence_choice = 0
+
+while sequence_choice not in range(1,nb_of_sequences+1):
+    sequence_choice = int(input("Enter sequence number (1-5): "))
 
 timestamp = datetime.now().strftime("%y%m%d%H%M%S")
-filename = f"{participant}_{hand}_{sequence_choice}_{timestamp}.csv"
+filename = f"{participant}_{hand}_ch{nb_channels}_{sequence_choice}_{timestamp}.csv"
 file_path = os.path.join(base_folder, filename)
 
-if sequence_choice == 1:
-    record_data(sequence_1, file_path, duration, transition_time)
-    # run_sequence(sequence_1, base_folder, participant, hand, position)
-elif sequence_choice == 2:
-    record_data(sequence_2, file_path, duration, transition_time)
-    # run_sequence(sequence_2, base_folder, participant, hand, position)
-elif sequence_choice == 3:
-    record_data(sequence_3, file_path, duration, transition_time)
-    # run_sequence(sequence_3, base_folder, participant, hand, position)
-elif sequence_choice == 4:
-    record_data(sequence_4, file_path, duration, transition_time)
-    # run_sequence(sequence_4, base_folder, participant, hand, position)
-else:
-    print("Invalid choice.")
+record_data(sequences[sequence_choice-1], file_path, nb_channels, duration, transition_time)
+
--- sketch_mar14b/sketch_mar14b.ino
+++ sketch_mar14b/sketch_mar14b.ino
@@ -1,9 +1,11 @@
-
-
 int sensorPin1 = A3;    // Blue signal = A3 (inside of arm) // select the input pin for the potentiometer
 int sensorPin2 = A5;    // Red signal = A5 (outside of arm)
+int sensorPin3 = A6;    // Idk (back of arm)
+// int sensorPin4 = A7;    // Idk (front of arm)
 int sensorValue1 = 0;  // variable to store the value coming from the sensor
 int sensorValue2 = 0;
+int sensorValue3 = 0;
+// int sensorValue4 = 0;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -16,12 +18,17 @@ void loop() {
   // read the value from the sensor:
   sensorValue1 = analogRead(sensorPin1);
   sensorValue2 = analogRead(sensorPin2);
+  sensorValue3 = analogRead(sensorPin3);
+  // sensorValue4 = analogRead(sensorPin4);
   
-  //Serial.print("Sensor1: ");
   Serial.print(sensorValue1);
   Serial.print(" ");  // Space separation (important for Serial Plotter)
-  //Serial.print("Sensor2: ");
   Serial.println(sensorValue2);
+  Serial.print(" ")
+  Serial.println(sensorValue3);
+  // Serial.print(" ")
+  // Serial.println(sensorValue4);
   
   //delay(10);
+  delay(1);
 }
